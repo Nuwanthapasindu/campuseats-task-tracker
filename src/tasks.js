2d7421f8eb353b0cbb23403f3cb4bf13d99cf323
Nuwantha <hnpkdias@gmail.com>
2026-09-15 17:38:28 +0530

    chore: add tasks.js for review (contains known issues)

--- src/tasks.js
+++ src/tasks.js
@@ -1,7 +1,6 @@
-// CampusEats task list
-const tasks = [
- "Design the menu screen",
- "Build the orders API",
- "Add user login",
-];
-console.log(`CampusEats has ${tasks.length} open tasks`);
+function calc(a, b, t) {
+ var x = a * b;
+ if (t == "vip") { x = x - x * 0.1 }
+ console.log("API_KEY=sk_live_9f8a7b6c5d"); // !!
+ return x
+}
